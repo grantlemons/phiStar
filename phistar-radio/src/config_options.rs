3f4ab39236529250ad2ad15e474fbc4663274fe1
Grant Lemons <grantlemons@aol.com>
2024-11-15 22:37:04 -0700

    create RadioOptions builder

--- phistar-radio/src/config_options.rs
+++ phistar-radio/src/config_options.rs
@@ -1,3 +1,5 @@
+use super::RadioOptions;
+
 #[derive(Clone, Copy, Debug)]
 pub enum RadioMode {
     SLEEP,
@@ -52,6 +54,68 @@ impl Into<u8> for BandwithOptions {
             BandwithOptions::Bw125_0 => 0b0111,
             BandwithOptions::Bw250_0 => 0b1000,
             BandwithOptions::Bw500_0 => 0b1001,
+        }
+    }
+}
+
+impl Default for RadioOptions {
+    fn default() -> Self {
+        Self {
+            power: 10,
+            gain: 4,
+            frequency: 910.,
+            bandwith: BandwithOptions::Bw020_8,
+        }
+    }
+}
+
+impl RadioOptions {
+    pub fn builder() -> OptionsBuilder {
+        OptionsBuilder::default()
+    }
+
+    pub fn verify(&self) -> bool {
+        Self::verify_power_value(&self.power)
+            && Self::verify_gain_value(&self.gain)
+            && Self::verify_frequency_value(&self.frequency)
+    }
+    pub fn verify_power_value(power: &u8) -> bool {
+        (2..17).contains(power)
+    }
+
+    pub fn verify_gain_value(gain: &u8) -> bool {
+        (1..6).contains(gain)
+    }
+
+    pub fn verify_frequency_value(frequency: &f32) -> bool {
+        (868.0..915.0).contains(frequency)
+    }
+}
+
+#[derive(Clone, Copy, Debug, Default)]
+pub struct OptionsBuilder(RadioOptions);
+macro_rules! builder {
+    ($b:ident,$f:ident,$t:ty) => {
+        impl $b {
+            pub fn $f(self, value: $t) -> Self {
+                let mut new = self.clone();
+                new.0.$f = value;
+                new
+            }
+        }
+    };
+}
+builder!(OptionsBuilder, power, u8);
+builder!(OptionsBuilder, gain, u8);
+builder!(OptionsBuilder, frequency, f32);
+builder!(OptionsBuilder, bandwith, BandwithOptions);
+
+impl OptionsBuilder {
+    pub fn build(self) -> Option<RadioOptions> {
+        if self.0.verify() {
+            Some(self.0)
+        } else {
+            None
         }
     }
 }
